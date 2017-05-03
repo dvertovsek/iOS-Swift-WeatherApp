67617d7e59651860b4bb2f45fd0d00297ebe30c6
Darijan <darijan.vertovsek@infinum.hr>
2017-05-03 22:12:32 +0200

    added map view controller to project

---
+++ WeatherApp/Map/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  WeatherApp
+//
+//  Created by Dare on 03/05/2017.
+//  Copyright © 2017 Dare. All rights reserved.
+//
+
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
